e69265d64c951ee9186ee8c467968d3670dd5528
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-27 16:17:31 -0700

    Add play one round function

--- main.js
+++ main.js
@@ -7,13 +7,47 @@ function computerMove() {
 function playerMove() {
   const input = prompt('Rock, Paper or Scissors: ').toLowerCase();
 
-  if ([possibleMoves.includes(input)]) {
+  if (possibleMoves.includes(input)) {
     return input;
   } else {
     alert('Invalid move, must be Rock, Paper or Scissors');
     return;
   }
 }
+
+function playRound(computerInput, playerInput) {
+  if (playerInput == 'rock') {
+    if (computerInput == 'paper') {
+      return 'Paper beats Rock! You lost';
+    } else if (computerInput == 'scissors') {
+      return 'You won! Rock beats Scissors';
+    } else {
+      return 'You both had Rock, its a tie';
+    }
+  } else if (playerInput == 'paper') {
+    if (computerInput == 'scissors') {
+      return 'Scissors beats Paper! You lost';
+    } else if (computerInput == 'rock') {
+      return 'You won! Paper beats Rock';
+    } else {
+      return 'You both had Paper, its a tie';
+    }
+  } else {
+    if (computerInput == 'rock') {
+      return 'Rock beats Scissors! You lost';
+    } else if (computerInput == 'paper') {
+      return 'You won! Scissors beats Paper';
+    } else {
+      return 'You both had Scissors, its a tie';
+    }
+  }
+}
+
+let playerInput = playerMove();
+let computerInput = computerMove();
+
+console.log(computerInput + ' ' + playerInput);
+console.log(playRound(computerInput, playerInput));
 
 // Rock beats scissors
 // Paper beats rock
