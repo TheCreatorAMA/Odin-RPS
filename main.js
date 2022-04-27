d69bdb040dbc4bfc6807a2efdc7599ed79d1431f
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-27 10:01:59 -0700

    Place possible moves in global scope so its accessible by all functions and create function for player move

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-function computerMove() {
-  let possibleMoves = ['Rock', 'Paper', 'Scissors'];
+const possibleMoves = ['rock', 'paper', 'scissors'];
 
+function computerMove() {
   return possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+}
+
+function playerMove() {
+  const input = prompt('Rock, Paper or Scissors: ').toLowerCase();
+
+  if ([possibleMoves.includes(input)]) {
+    return input;
+  } else {
+    alert('Invalid move, must be Rock, Paper or Scissors');
+    return;
+  }
 }
 
 // Rock beats scissors
 // Paper beats rock
 // Scissors beats paper
+
+// Plan:
+// get user input and check if its valid
+//  - can only be rock paper or scissors
+// get computer move
+// compare who wins
+// output to user winner
