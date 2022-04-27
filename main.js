eacf9f6d1417c1c1f0952c282ed097d95d2a9395
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-27 09:44:24 -0700

    Add function to randomly determine computer move

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('HELLO');
+function computerMove() {
+  let possibleMoves = ['Rock', 'Paper', 'Scissors'];
+
+  return possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+}
+
+// Rock beats scissors
+// Paper beats rock
+// Scissors beats paper
